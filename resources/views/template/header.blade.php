683765861e28baa478e1030a7f3ba8541122844f
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2021-07-30 23:03:37 +0700

    href fix

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -2,13 +2,13 @@
 <!-- NAVBAR -->
 <nav class="navbar navbar-expand-lg bg  fixed-top tambahan-ku" id="mainNav">
   <div class="container-fluid" style="margin-right: 3%; margin-left: 3%;">
-      <a class="navbar-brand js-scroll-trigger page-scroll" href="#"><img src="{{ asset('assets/img/Logo-TK-outline.png') }}" width="70px"></a>
+      <a class="navbar-brand js-scroll-trigger page-scroll" href="../"><img src="{{ asset('assets/img/Logo-TK-outline.png') }}" width="70px"></a>
       <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold animo" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <i class="fas fa-bars"></i>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive" style="text-decoration: none;">
           <ul class="navbar-nav ml-auto customm">
-              <li class="nav-item mx-0 mx-lg-1 garis-navbar "><a class="nav-link py-3 px-0 px-lg-3 rounded" href="home.php" style="color: #fff;">Beranda</a></li>
+              <li class="nav-item mx-0 mx-lg-1 garis-navbar "><a class="nav-link py-3 px-0 px-lg-3 rounded" href="../" style="color: #fff;">Beranda</a></li>
               
               <li class="nav-item mx-0 mx-lg-1 garis-navbar dropdown show "><a class="nav-link py-3 px-0 px-lg-3 rounded dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#" style="color: #fff;">Profile</a>
                 <div class="dropdown-menu resp" aria-labelledby="dropdownMenuLink">
@@ -36,7 +36,7 @@
                     <a class="dropdown-item resp" href="#">EVCON</a>
                   </div>
               </li>
-              <li class="nav-item mx-0 mx-lg-1 garis-navbar  "><a class="nav-link py-3 px-0 px-lg-3 rounded" href="kontak-kami.php" style="color: #fff;">Kontak</a></li>
+              <li class="nav-item mx-0 mx-lg-1 garis-navbar  "><a class="nav-link py-3 px-0 px-lg-3 rounded" href="./kontak-kami" style="color: #fff;">Kontak</a></li>
             </ul>
       </div>
   </div>
