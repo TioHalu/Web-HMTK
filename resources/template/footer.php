f8e58262138039b4b2e00bb84f78150d1e4f8aec
habibmahaasin <habibirf23@gmail.com>
2021-07-30 20:30:41 +0700

    add Header dan footer

---
+++ resources/template/footer.php
@@ -0,0 +1,28 @@
+    <footer class="text-center text-lg-start sosmedd" style="margin-top: 150px;">
+      <div class="text-center p-3">
+        <h4><b>Our Social Media</b></h4>
+        <!-<a href="#"><i class="fab fa-line"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+      </div>
+    </footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    
+<!-- NAVBAR SCROLLING GANTI WARNA-->
+<script type="text/javascript">
+            $(window).on("scroll", function(){
+                if($(window).scrollTop() > 100){
+                    $(".tambahan-ku").addClass("active");
+                } else {
+                    $(".tambahan-ku").removeClass("active");
+                }
+            });
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  
